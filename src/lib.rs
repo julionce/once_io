584f0abd8a3a3eafe87ac430560495125646ecf0
julionce <julianbortega@gmail.com>
2023-08-29 19:31:13 +0200

    Add Stream trait and implement it

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[cfg(test)]
+extern crate static_assertions as sa;
+
+use std::io::{Read, Seek, Write};
+
+pub trait Stream: Read + Write + Seek {}
+
+impl<T> Stream for T where T: Read + Write + Seek {}
 
 #[cfg(test)]
 mod tests {
+    use std::io::Cursor;
+
+    use sa::assert_impl_all;
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn assert_stream_impl() {
+        assert_impl_all!(dyn Stream: Read, Write, Seek);
+        assert_impl_all!(Cursor<Vec<u8>>: Read, Write, Seek, Stream);
     }
 }
